f4938c712abb825807833de7c620a499c69a2ffa
Vitali Griunvald <vgriunvald@gmail.com>
2021-01-21 20:26:06 +0200

    Add auth reducer and actions creator

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import testReducer from './testReducer';
 import loginModalReducer from './loginModalReducer';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   test: testReducer,
   loginModal: loginModalReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
