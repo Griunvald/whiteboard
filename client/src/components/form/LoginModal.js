0ecce1c3aa2eb4a277eed3a80180832f87e3ee86
Vitali Griunvald <vgriunvald@gmail.com>
2021-01-21 20:02:55 +0200

    Add login form

--- client/src/components/form/LoginModal.js
+++ client/src/components/form/LoginModal.js
@@ -1,23 +1,58 @@
 import React from 'react';
 import { Modal, Button } from 'semantic-ui-react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import CustomInput from '../common/form/CustomInput';
 const LoginModal = () => {
   const open = useSelector((state) => state.loginModal.open);
   const dispatch = useDispatch();
   return (
     <div>
-      <Modal open={open} onClose={() => dispatch({ type: 'CLOSE_MODAL' })}>
+      <Modal
+        open={open}
+        onClose={() => dispatch({ type: 'CLOSE_MODAL' })}
+        size="mini"
+      >
         <Modal.Header>
           <h2>Login modal</h2>
         </Modal.Header>
-        <Modal.Content></Modal.Content>
+        <Modal.Content>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={Yup.object({
+              email: Yup.string().required().email(),
+              password: Yup.string().required(),
+            })}
+            onSubmit={(values) => console.log(values)}
+          >
+            {({ isSubmitting, isValid, dirty }) => (
+              <Form className="ui form">
+                <CustomInput
+                  name="email"
+                  placeholder="Email address"
+                ></CustomInput>
+                <CustomInput
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                ></CustomInput>
+                <Button
+                  loading={isSubmitting}
+                  disabled={!isValid || !dirty || isSubmitting}
+                  type="submit"
+                  fluid
+                  size="large"
+                  color="green"
+                  content="Login"
+                />
+              </Form>
+            )}
+          </Formik>
+        </Modal.Content>
         <Modal.Actions>
           <Button negative onClick={() => dispatch({ type: 'CLOSE_MODAL' })}>
             Close
-          </Button>
-          <Button positive onClick={() => dispatch({ type: 'CLOSE_MODAL' })}>
-            Login
           </Button>
         </Modal.Actions>
       </Modal>
