8641aff870acc30500c248d0934c3d45db11bf83
Vitali Griunvald <vgriunvald@gmail.com>
2021-01-22 18:58:33 +0200

    Install and configure redux-thunk

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,9 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/rootReducer';
+import thunk from 'redux-thunk';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const configureStore = () => {
-  return createStore(rootReducer, devToolsEnhancer());
+  return createStore(
+    rootReducer,
+    compose(applyMiddleware(thunk), devToolsEnhancer())
+  );
 };
 
 export default configureStore;
