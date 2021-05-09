50441e70a9f625942fa749ba276c3dc22ec1c9d3
Vitali Griunvald <vgriunvald@gmail.com>
2021-05-09 21:52:03 +0300

    Add .env file, update firebaseConfig

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCD6p6d245BaUHvWawO8mD4-YLYa7F3phA',
-  authDomain: 'whiteboard-v2.firebaseapp.com',
-  projectId: 'whiteboard-v2',
-  storageBucket: 'whiteboard-v2.appspot.com',
-  messagingSenderId: '253544553674',
-  appId: '1:253544553674:web:511001c90fc8b688f46833',
-  measurementId: 'G-JV03DZJE27',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
